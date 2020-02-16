9f591cfd17fd316f7415203e0932748c0c2ee0a7
meownized <meownized@gmail.com>
2020-02-16 21:11:09 +0300

    added couple params to users

---
+++ db/migrate/20200216180945_add_couple_to_user.rb
@@ -0,0 +1,5 @@
+class AddCoupleToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :couple, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_180306) do
+ActiveRecord::Schema.define(version: 2020_02_16_180945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2020_02_16_180306) do
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "couple"
   end
 
 end
