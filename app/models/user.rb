05f5e7d584e34130e65fbae11b5503c15bf81dd0
meownized <meownized@gmail.com>
2020-03-14 16:29:56 +0300

    fixed avatar uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :rents
-  has_many :flats, through: :rents
-  has_many :rooms, through: :rents
+  has_many :flats, through: :rents, dependent: :destroy
+  has_many :rooms, through: :rents, dependent: :destroy
 
   has_many :messages
   has_many :message_statuses
