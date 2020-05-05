130024903b55a3a8f56bfe909819fb8fd9338c8a
meownized <meownized@gmail.com>
2020-05-05 20:54:38 +0300

    css beautify

--- app/javascript/components/ConversationList.jsx
+++ app/javascript/components/ConversationList.jsx
@@ -2,33 +2,31 @@ import React from "react"
 import PropTypes from "prop-types"
 
 class ConversationList extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.handleClick = this.handleClick.bind(this)
-  }
+		this.handleClick = this.handleClick.bind(this)
+	}
 
-  handleClick = id => {
-    this.props.handleClick(id)
-  };
+	handleClick = id => {
+		this.props.handleClick(id)
+	};
 
-  conversationsList() {
-    const conversations = this.props.conversations
+	conversationsList() {
+		const conversations = this.props.conversations
 
-    return conversations.map((conversation, index) =>
-      <div className="col-sm-12" key={index} onClick={() => this.handleClick(conversation.id)}>
-        <p className="message-text">
-          {conversation.title}
-        </p>
-      </div>
-    );
-  }
+		return conversations.map((conversation, index) => <div className="col-sm-12" key={index} onClick={() => this.handleClick(conversation.id)}>
+			<p className="message-text">
+				{conversation.title}
+			</p>
+		</div>);
+	}
 
-  render() {
-    return <div className="row" id="chat-box">
-      {this.conversationsList()}
-    </div>
-  }
+	render() {
+		return <div className="row" id="chat-box">
+			{this.conversationsList()}
+		</div>
+	}
 }
 
 export default ConversationList
