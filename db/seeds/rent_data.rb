5dd804b1e6006f32b3fdf301cb6260aac7316324
meownized <meownized@gmail.com>
2020-05-26 18:01:12 +0300

    active storage for seeds

--- db/seeds/rent_data.rb
+++ db/seeds/rent_data.rb
@@ -7,6 +7,9 @@ def create_rents
     flat.rooms.each do |room|
       Rent.where(flat_id: flat.id, room_id: room.id).each do |rent|
         user = User.create(basic_info)
+        time = rand(0..3)
+        user.avatar.attach(filename: "#{time}.jpg", io: File.open("db/images/avatars/#{time}.jpg", content_type: "image/jpg"))
+
         rent.update(user_id: user.id)
         flat.conversations.each do |conversation|
           conversation.users << user
