6f1421e702f18a224c676e45353b5f65b1f1dd3e
meownized <meownized@gmail.com>
2020-05-28 12:16:52 +0300

    changed action for registration, show and edit

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  get 'users/profile/:id', to: 'users#profile'
+  get 'users/registration/:id', to: 'users#registration'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
