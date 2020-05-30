81bfb614a4744e75330e9f52b0c018419aa068fb
Server <meownized@gmail.com>
2020-05-30 08:28:08 +0200

    server update

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,9 @@ Bundler.require(*Rails.groups)
 
 module Smate
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
+    config.hosts << "smate.life"
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators.system_tests = nil
   end
 end
